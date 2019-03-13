03858dd3f05f9b5240ff2bf894ea7e01e51274fe
Lorenzo Llamas <ltl924@gmail.com>
2019-03-13 01:51:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,8 @@ def speak_to_grandma(phrase)
     return 'NO, NOT SINCE 1938'
   else
     return 'HUH?! SPEAK UP, SONNY!'
+  end
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
